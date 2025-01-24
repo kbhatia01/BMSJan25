626800f9745ec13e0256ad9ffe1c6267a962c069
kbhatia01 <karanbhtiacs@gmail.com>
2025-01-24 18:59:59 +0530

    add models

---
+++ BMSPyApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BMSPyApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BMSPyApp.settings')
+
+application = get_asgi_application()
